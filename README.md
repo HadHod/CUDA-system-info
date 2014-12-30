ebc172b7b48d3d4134168db7b650ef87b23a73e8
hadhod <maciej.mlynarski@gmail.com>
2014-12-30 10:57:25 +0100

    Added new positions & README update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-CUDA system info ver 1.0
-========================
+CUDA system info v1.1
+======================
+
+Informations:
+* DRIVER VERSION
+* RUNTIME VERSION
+* TOTAL MEMORY
+* FREE MEMORY
+* Number of devices
+
+Devices:
+* Name
+* Clock rate
+* Device compute capability
+* Multiprocessor count
+* Total global mem
+* Max threads per MProcessor
+* Shared memory per Block
+* Warp size
+* Max threads pen Block
+* Max block dimension
+* Max grid dimension
+* Kernel execution timeout
+* Device overlap
--- main.cu
+++ main.cu
@@ -35,9 +35,13 @@ int main() {
         cout << "Multiprocessor count: " << prop.multiProcessorCount << "\n";
         cout << "Total global mem: " << prop.totalGlobalMem * 1e-6f << "MB\n";
         cout << "Max threads per MProcessor: " << prop.maxThreadsPerMultiProcessor << "\n";
-        cout << "Max threads pen Block: " << prop.maxThreadsPerBlock << "\n";
         cout << "Shared memory per Block: " << prop.sharedMemPerBlock * 1e-6f << "MB\n";
         cout << "Warp size: " << prop.warpSize << "\n";
+        cout << "Max threads pen Block: " << prop.maxThreadsPerBlock << "\n";
+        cout << "Max block dimension: (" << prop.maxThreadsDim[0] << ", " << prop.maxThreadsDim[1] << ", " << prop.maxThreadsDim[2] << ")\n";
+        cout << "Max grid dimension: (" << prop.maxGridSize[0] << ", " << prop.maxGridSize[1] << ", " << prop.maxGridSize[2] << ")\n";
+        cout << "Kernel execution timeout is " << (prop.kernelExecTimeoutEnabled ? "on" : "off") << "\n";
+        cout << "Device overlap is " << (prop.deviceOverlap ? "on" : "off") << "\n";
     }
 
     cout << endl;
